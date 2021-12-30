a55fec08fa80c123d3ff6ac723ea4b8d4bb2f39e
Alex Larioza <contact@alexlarioza.com>
2021-12-30 09:10:44 -0800

    adding messages to all asserts to make it clear what's being tested + some minor organizational things

--- tests.lua
+++ tests.lua
@@ -1,11 +1,19 @@
+-- ██████╗ ██╗   ██╗██╗██╗     ██████╗ 
+-- ██╔══██╗██║   ██║██║██║     ██╔══██╗
+-- ██████╔╝██║   ██║██║██║     ██║  ██║
+-- ██╔══██╗██║   ██║██║██║     ██║  ██║
+-- ██████╔╝╚██████╔╝██║███████╗██████╔╝
+-- ╚═════╝  ╚═════╝ ╚═╝╚══════╝╚═════╝ 
+-- build must happen first so that the correct version of the playbit module is loaded
+
 local build = require("build")
 
--- TODO: break build step out so there can be separate tests for different configurations e.g. playdate vs love2d
 build.build({ 
   verbose = false,
   assert = true,
   debug = true,
-  platform = "playdate",
+  -- we don't really need to test actual platform specific APIs, so just compile those out
+  platform = "fakeplatformfortesting",
   output = "_test\\",
   luaFolders = {
     { "playbit", "playbit" }
@@ -22,7 +30,14 @@ package.path = ".\\_test\\?.lua;" .. package.path
 
 -- set playbit globally
 pb = require("_test.playbit.pb")
-local components = require("_test.playbit.components")
+
+-- ████████╗███████╗███████╗████████╗███████╗
+-- ╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝██╔════╝
+--    ██║   █████╗  ███████╗   ██║   ███████╗
+--    ██║   ██╔══╝  ╚════██║   ██║   ╚════██║
+--    ██║   ███████╗███████║   ██║   ███████║
+--    ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚══════╝
+-- Tests for various systems. Should be updated regularly. ads 
 
 function CreateAppInstance()
   local game = pb.app.new()
@@ -36,31 +51,32 @@ function CreateAppInstance()
   return game
 end
 
-function Test_Systems()
+-- Test basic system functionality
+function Test_System()
   local game = pb.app.new()
 
   local componentAId = game:registerComponent("ComponentA", {})
   local componentBId = game:registerComponent("ComponentB", {})
   local componentCId = game:registerComponent("ComponentC", {})
-  assert(componentAId == 1)
-  assert(componentBId == 2)
-  assert(componentCId == 3)
-
-  local systemAId = game:registerSystem({ name="SystemA",   components={ "ComponentA" } })
-  local systemBId = game:registerSystem({ name="SystemB",   components={ "ComponentB" } })
-  local systemCId = game:registerSystem({ name="SystemC",   components={ "ComponentC" } })
-  local systemBCId = game:registerSystem({ name="SystemBC", components={ "ComponentB", "ComponentC" } })
-  assert(systemAId == 1)
-  assert(systemBId == 2)
-  assert(systemCId == 3)
-  assert(systemBCId == 4)
+  assert(componentAId == 1, "Component ID is expected to be 1.")
+  assert(componentBId == 2, "Component ID is expected to be 2.")
+  assert(componentCId == 3, "Component ID is expected to be 3.")
   
   for i = 1, 24, 1 do
     game:registerComponent(tostring(i), {})
   end
 
   local componentDId = game:registerComponent("ComponentD", {})
-  assert(componentDId == 28)
+  assert(componentDId == 28, "Component ID is expected to be 28.")
+
+  local systemAId = game:registerSystem({ name="SystemA",   components={ "ComponentA" } })
+  local systemBId = game:registerSystem({ name="SystemB",   components={ "ComponentB" } })
+  local systemCId = game:registerSystem({ name="SystemC",   components={ "ComponentC" } })
+  local systemBCId = game:registerSystem({ name="SystemBC", components={ "ComponentB", "ComponentC" } })
+  assert(systemAId == 1, "System ID is expected to be 1.")
+  assert(systemBId == 2, "System ID is expected to be 2.")
+  assert(systemCId == 3, "System ID is expected to be 3.")
+  assert(systemBCId == 4, "System ID is expected to be 4.")
 
   local scene = pb.scene.new(game)
   game:changeScene(scene)
@@ -81,12 +97,13 @@ function Test_Systems()
 
   scene:update()
   
-  assert(#scene.systemEntityIds[systemAId].entities == 1)
-  assert(#scene.systemEntityIds[systemBId].entities == 2)
-  assert(#scene.systemEntityIds[systemCId].entities == 2)
-  assert(#scene.systemEntityIds[systemBCId].entities == 1)
+  assert(#scene.systemEntityIds[systemAId].entities == 1, "System A should match with 1 entity.")
+  assert(#scene.systemEntityIds[systemBId].entities == 2, "System B should match with 2 entities.")
+  assert(#scene.systemEntityIds[systemCId].entities == 2, "System C should match with 2 entities.")
+  assert(#scene.systemEntityIds[systemBCId].entities == 1, "System BC should match with 1 entity.")
 end
 
+-- Tests misc util functions
 function Test_Utils()
   local tableA = {
     x=0,
@@ -95,25 +112,26 @@ function Test_Utils()
   }
   local tableB = {}
   pb.util.shallowCopy(tableB, tableA)
-  assert(tableB.x == tableA.x)
-  assert(tableB.y == tableA.y)
-  assert(tableB.z == tableA.z)
+  assert(tableB.x == 0, "TableB's x value should equal TableA's x value of 0.")
+  assert(tableB.y == 1004, "TableB's y value should equal TableA's y value of 1004.")
+  assert(tableB.z == 20, "TableB's z value should equal TableA's z value of 20.")
 
   tableA.x = 40000
-  assert(tableB.x ~= tableA.x)
+  assert(tableB.x == 0, "TableB.x should still be 0 and not change when TableA.x is set.")
 
   tableB.x = 1000
-  assert(tableB.x ~= tableA.x)
+  assert(tableA.x == 40000, "TableA.x should still be 40000 and not change when TableB.x is set.")
 end
 
+-- Tests bitfields
 function Test_Bitfield()
   local emptyBitfield = pb.bitfield.new()
-  assert(emptyBitfield.value == 0)
-  assert(not emptyBitfield:has(0))
-  assert(not emptyBitfield:has(1))
-  assert(not emptyBitfield:has(10))
-  assert(not emptyBitfield:has(26))
-  assert(not emptyBitfield:has(31))
+  assert(emptyBitfield.value == 0, "Value should be 0 with no flags set.")
+  assert(not emptyBitfield:has(0), "Flag 0 should not be set.")
+  assert(not emptyBitfield:has(1), "Flag 1 should not be set.")
+  assert(not emptyBitfield:has(10), "Flag 10 should not be set.")
+  assert(not emptyBitfield:has(26), "Flag 26 should not be set.")
+  assert(not emptyBitfield:has(31), "Flag 31 should not be set.")
 
   local allFlags = {}
   for i = 1, 32, 1 do
@@ -121,12 +139,12 @@ function Test_Bitfield()
   end
 
   local fullBitfield = pb.bitfield.new(allFlags)
-  assert(fullBitfield.value == 4294967295)
-  assert(fullBitfield:has(0))
-  assert(fullBitfield:has(1))
-  assert(fullBitfield:has(10))
-  assert(fullBitfield:has(26))
-  assert(fullBitfield:has(31))
+  assert(fullBitfield.value == 4294967295, "Value should be 4294967295 with all flags set.")
+  assert(fullBitfield:has(0), "Flag 0 should be set.")
+  assert(fullBitfield:has(1), "Flag 1 should be set.")
+  assert(fullBitfield:has(10), "Flag 10 should be set.")
+  assert(fullBitfield:has(26), "Flag 26 should be set.")
+  assert(fullBitfield:has(31), "Flag 31 should be set.")
 
   local evenFlags = {}
   for i = 0, 31, 1 do
@@ -136,68 +154,65 @@ function Test_Bitfield()
   end
 
   local evenBitfield = pb.bitfield.new(evenFlags)
-  assert(evenBitfield.value == 1431655765)
-  assert(evenBitfield:has(0))
-  assert(not evenBitfield:has(1))
-  assert(evenBitfield:has(2))
-  assert(not evenBitfield:has(21))
-  assert(evenBitfield:has(30))
+  assert(evenBitfield.value == 1431655765, "Value should be 1431655765 with all even flags set.")
+  assert(evenBitfield:has(0), "Flag 0 should be set.")
+  assert(not evenBitfield:has(1), "Flag 1 should not be set.")
+  assert(evenBitfield:has(2), "Flag 2 should be set.")
+  assert(not evenBitfield:has(21), "Flag 21 should not be set.")
+  assert(evenBitfield:has(30), "Flag 30 should be set.")
 
   local bitfield = pb.bitfield.new()
-  assert(bitfield.value == 0)
-  assert(not bitfield:has(0))
-  assert(not bitfield:has(7))
-  assert(not bitfield:has(31))
-  
   bitfield:set(31)
-  assert(bitfield.value == 2147483648);
-  assert(not bitfield:has(0))
-  assert(not bitfield:has(7))
-  assert(bitfield:has(31))
+  assert(bitfield.value == 2147483648, "Value should be 2147483648 with flag 31 set.")
+  assert(not bitfield:has(0), "Flag 0 should not be set.")
+  assert(not bitfield:has(7), "Flag 7 should not be set.")
+  assert(bitfield:has(31), "Flag 31 should be set.")
 
   bitfield:set(7)
-  assert(bitfield.value == 2147483776);
-  assert(not bitfield:has(0))
-  assert(bitfield:has(7))
-  assert(bitfield:has(31))
+  assert(bitfield.value == 2147483776, "Value should be 2147483776 with flag 31 and 7 set.")
+  assert(not bitfield:has(0), "Flag 0 should not be set.")
+  assert(bitfield:has(7), "Flag 7 should be set.")
+  assert(bitfield:has(31), "Flag 31 should be set.")
 
   bitfield:unset(31)
-  assert(bitfield.value == 128);
-  assert(not bitfield:has(0))
-  assert(bitfield:has(7))
-  assert(not bitfield:has(31))
+  assert(bitfield.value == 128, "Value should be 128 with flag 7 set.")
+  assert(not bitfield:has(0), "Flag 0 should not be set.")
+  assert(bitfield:has(7), "Flag 7 should be set.")
+  assert(not bitfield:has(31), "Flag 31 should not be set.")
 
   bitfield:unset(7)
-  assert(bitfield.value == 0)
-  assert(not bitfield:has(0))
-  assert(not bitfield:has(7))
-  assert(not bitfield:has(31))
+  assert(bitfield.value == 0, "Value should be 0 with no flags set.")
+  assert(not bitfield:has(0), "Flag 0 should not be set.")
+  assert(not bitfield:has(7), "Flag 7 should not be set.")
+  assert(not bitfield:has(31), "Flag 31 should not be set.")
 end
 
-function Test_Components()
+-- Test component arrays
+function Test_ComponentArrays()
   local componentArray = require("playbit.component-array")
   local arr = componentArray.new()
 
   arr:add(0, { value="A"})
   arr:add(1, { value="B"})
   arr:add(2, { value="C"})
-  assert(#arr.components == 3)
+  assert(#arr.components == 3, "Adding components should increased component count.")
 
   arr:remove(1)
-  assert(#arr.components == 2)
-  assert(arr:get(2).value == "C")
+  assert(#arr.components == 2, "Removing a component should reduce component count.")
+  assert(arr:get(2).value == "C", "Removing a component should not affect other components.")
 
   arr:add(3, { value="D"})
-  assert(#arr.components == 3)
-  assert(arr:get(2).value == "C")
-  assert(arr:get(3).value == "D")
+  assert(#arr.components == 3, "Adding another component should increased component count.")
+  assert(arr:get(2).value == "C", "Adding another component should not affect other components.")
+  assert(arr:get(3).value == "D", "Adding another component should not affect other components.")
 
   arr:remove(0)
   arr:remove(2)
   arr:remove(3)
-  assert(#arr.components == 0)
+  assert(#arr.components == 0, "Removing all components should result in empty array.")
 end
 
+-- Tests entities
 function Test_Entities()
   local game = CreateAppInstance()
   local nameId = game:getComponentId("name")
@@ -214,15 +229,15 @@ function Test_Entities()
     [pb.components.Transform.name] = { x = 5 },
   })
   scene:update()
-  assert(scene.entityCount == 1)
+  assert(scene.entityCount == 1, "Adding an entity should increase entity count.")
 
-  local tranformComponent = scene:getComponent(tempId, pb.components.Transform.name)
-  assert(tranformComponent.x == 5)
-  assert(tranformComponent.y == 0)
+  local transformComponent = scene:getComponent(tempId, pb.components.Transform.name)
+  assert(transformComponent.x == 5, "x value should be the original value of 5.")
+  assert(transformComponent.y == 0, "y value was not set and should be 0.")
 
   scene:removeEntity(tempId)
   scene:update()
-  assert(scene.entityCount == 0)
+  assert(scene.entityCount == 0, "Removing an entity should reduce entity count.")
 
   local playerName = "Player"
   local playerId = scene:addEntity({ 
@@ -231,79 +246,80 @@ function Test_Entities()
   })
   scene:update()
 
-  local nameSystemId = game:getSystemId("name-allocator")
-  assert(#scene.systemEntityIds[nameSystemId].entities == 1)
-  assert(playerId == 1)
-  assert(scene.entityCount == 1)
-  assert(scene:findEntity(playerName) == playerId)
-  assert(scene:findEntity("does not exist") == -1)
+  assert(playerId == 1, "PlayerId should be 1.")
+  assert(scene:findEntity(playerName) == playerId, "Returned entity id should match player id of 1.")
+  assert(scene:findEntity("does not exist") == -1, "Entity should not be able to be found and return special value of -1.")
 
-  assert(scene:hasComponentId(playerId, nameId))
-  assert(scene:hasComponentId(playerId, componentAId))
-  assert(not scene:hasComponentId(playerId, componentBId))
-  assert(not scene:hasComponentId(playerId, transformId))
+  assert(scene:hasComponentId(playerId, nameId), "Player entity should have name component.")
+  assert(scene:hasComponentId(playerId, componentAId), "Player entity should have ComponentA.")
+  assert(not scene:hasComponentId(playerId, componentBId), "Player entity should not have ComponentB.")
+  assert(not scene:hasComponentId(playerId, transformId), "Player entity should not have transform component.")
   
   scene:addComponent(playerId, "ComponentB", {})
   scene:update()
 
-  assert(scene:hasComponentId(playerId, nameId))
-  assert(scene:hasComponentId(playerId, componentAId))
-  assert(scene:hasComponentId(playerId, componentBId))
-  assert(not scene:hasComponentId(playerId, transformId))
+  assert(scene:hasComponentId(playerId, componentBId), "Player entity should now have ComponentB.")
 
   scene:removeComponent(playerId, "ComponentA")
   scene:update()
 
-  assert(scene:hasComponentId(playerId, nameId))
-  assert(not scene:hasComponentId(playerId, componentAId))
-  assert(scene:hasComponentId(playerId, componentBId))
-  assert(not scene:hasComponentId(playerId, transformId))
+  assert(scene:hasComponentId(playerId, nameId), "Removing ComponentA should not have removed name component.")
+  assert(not scene:hasComponentId(playerId, componentAId), "Player entity should have had ComponentA removed.")
+  assert(scene:hasComponentId(playerId, componentBId), "Removing ComponentA should not have removed ComponentB.")
 
   local entityA = scene:addEntity({ 
     ["ComponentA"] = { x=0, y=0 },
   })
   scene:update()
-  assert(entityA == 2)
-  assert(scene.entityCount == 2)
+  assert(entityA == 2, "Adding a second entity should have result in an id of 2.")
+  assert(scene.entityCount == 2, "Adding a new entity should have increased entity count.")
 
   local entityB = scene:addEntity({ 
     ["ComponentA"] = { x=0, y=0 },
   })
   scene:update()
-  assert(entityB == 3)
-  assert(scene.entityCount == 3)
+  assert(entityB == 3, "Adding a second entity should have result in an id of 3.")
+  assert(scene.entityCount == 3, "Adding a new entity should have increased entity count.")
 
   scene:removeEntity(entityA)
   scene:update()
-  assert(scene.entityCount == 2)
+  assert(scene.entityCount == 2, "Removing an entity should have reduced entity count.")
 
   local entityC = scene:addEntity({ 
     ["ComponentA"] = { x=0, y=0 },
   })
   scene:update()
-  assert(entityC == 2)
-  assert(scene.entityCount == 3)
+  assert(entityC == 2, "Adding a third entity after one was removed should have result in the reuse of entity id 2.")
+  assert(scene.entityCount == 3, "Adding a new entity should have increased entity count.")
 
   scene:removeEntity(playerId)
   scene:update()
-  assert(scene.entityCount == 2)
+  assert(scene.entityCount == 2, "Removing an entity should have reduced entity count.")
 
-  assert(scene:findEntity(playerName) == -1)
+  assert(scene:findEntity(playerName) == -1, "Removing the player should have caused the entity to not have been found by name.")
 end
 
 function Test_App()
   local game = CreateAppInstance()
 
   local nameId = game:getComponentId("name")
-  assert(nameId ~= nil)
+  assert(nameId ~= nil, "Component id should have been returned.")
 end
+
+-- ██████╗ ██╗   ██╗███╗   ██╗
+-- ██╔══██╗██║   ██║████╗  ██║
+-- ██████╔╝██║   ██║██╔██╗ ██║
+-- ██╔══██╗██║   ██║██║╚██╗██║
+-- ██║  ██║╚██████╔╝██║ ╚████║
+-- ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+-- Run all test cases and print results                          
 
 local testsToRun = {
   { name="App Tests", testFunction=Test_App },
   { name="Bitfield Tests", testFunction=Test_Bitfield },
   { name="Misc Util Tests", testFunction=Test_Utils },
-  { name="System Tests", testFunction=Test_Systems },
-  { name="Component Tests", testFunction=Test_Components },
+  { name="System Tests", testFunction=Test_System },
+  { name="Component Tests", testFunction=Test_ComponentArrays },
   { name="Entity Tests", testFunction=Test_Entities },
 }
 
