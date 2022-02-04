28f91a3a3dfa605329ef8ff8a3ffb77e2214e6a5
Alex Larioza <contact@alexlarioza.com>
2022-02-04 10:59:19 -0800

    added distance function that takes raw x/y pairs

--- playbit/vector.lua
+++ playbit/vector.lua
@@ -129,6 +129,12 @@ function Vector.distance(a, b)
   return (b - a):len()
 end
 
+function Vector.distanceXY(x1, y1, x2, y2)
+  local x = x1 - x2
+  local y = y1 - y2
+  return math.sqrt(x * x + y * y)
+end
+
 function Vector.len(x, y)
   return math.sqrt(x * x + y * y)
 end
