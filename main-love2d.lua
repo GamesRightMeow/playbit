fede5c59710d871fda377065c99406bc84b5a6a1
Alex Larioza <contact@alexlarioza.com>
2022-04-02 20:14:00 -0700

    added platform main scripts

---
+++ main-love2d.lua
@@ -0,0 +1,44 @@
+--! if DEBUG then
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+  require("lldebugger").start()
+end
+--! end
+
+require("playbit.pb")
+pb.import("scripts/game")
+
+function love.load()
+  pb.app.load()
+end
+
+function love.update()
+  pb.app.update()
+end
+
+function love.draw()
+  pb.app.render()
+end
+
+function love.joystickadded(joystick)
+  pb.input.handleJoystickAdded(joystick)
+end
+
+function love.joystickremoved(joystick)
+  pb.input.handleJoystickRemoved(joystick)
+end
+
+function love.gamepadreleased(joystick, button)
+  pb.input.handleGamepadReleased(joystick, button)
+end
+
+function love.gamepadpressed(joystick, button)
+  pb.input.handleGamepadPressed(joystick, button)
+end
+
+function love.keypressed(key)
+  pb.input.handleKeyPressed(key)
+end
+
+function love.keyreleased(key)
+  pb.input.handleKeyReleased(key)
+end
---
+++ main-playdate.lua
@@ -0,0 +1,60 @@
+import("playbit/pb")
+pb.import("scripts/game")
+
+pb.app.load()
+
+function playdate.update()
+  pb.app.update()
+
+  pb.app.render()
+
+  -- TODO: input
+end
+
+function playdate.AButtonDown()
+  pb.input.handleKeyPressed("a")
+end
+
+function playdate.AButtonUp()
+  pb.input.handleKeyReleased("a")
+end
+
+function playdate.BButtonDown()
+  pb.input.handleKeyPressed("b")
+end
+
+function playdate.BButtonUp()
+  pb.input.handleKeyReleased("b")
+end
+
+function playdate.upButtonDown()
+  pb.input.handleKeyPressed("up")
+end
+
+function playdate.upButtonUp()
+  pb.input.handleKeyReleased("up")
+end
+
+function playdate.downButtonDown()
+  pb.input.handleKeyPressed("down")
+end
+
+function playdate.downButtonUp()
+  pb.input.handleKeyReleased("down")
+end
+
+function playdate.leftButtonDown()
+  pb.input.handleKeyPressed("left")
+end
+
+function playdate.leftButtonUp()
+  pb.input.handleKeyReleased("left")
+end
+
+function playdate.rightButtonDown()
+  pb.input.handleKeyPressed("right")
+end
+
+function playdate.rightButtonUp()
+  pb.input.handleKeyReleased("right")
+end
