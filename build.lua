3d7a3d95fad0692602b1936c97f982afec5f11d8
Alex Larioza <contact@alexlarioza.com>
2022-04-08 20:46:50 -0700

    refactored build script

--- build.lua
+++ build.lua
@@ -1,125 +1,163 @@
 -- this build script was only designed to run on Windows!
 local folderOfThisFile = (...):match("(.-)[^%.]+$")
 local pp = require(folderOfThisFile.."LuaPreprocess.preprocess")
+local capToBmfont = require(folderOfThisFile.."tools.caps-to-bmfont")
 
-local Build = {}
+local module = {}
 
-function Build.listFiles(folder)
+function module.listFiles(folder)
   local dirCommand = io.popen("dir /a-D /S /B \""..folder.."\"")
   local output = dirCommand:read("*a")
   return output:gmatch("(.-)\n")
 end
 
-function Build.getFileExtension(path)
-  return path:match("^.+(%..+)$")
+function module.getFileExtension(path)
+  return path:match("^.+%.(.+)$")
 end
 
-function Build.createFolderIfNeeded(path)
-  local existCommand = io.popen("IF EXIST "..path.." ECHO true")
-  local existOutput = existCommand:read("*a")
-  if #existOutput == 0 then
-    os.execute("mkdir " .. path)
-  end
+function module.createFolderIfNeeded(path)
+  local pathReversed = string.reverse(path)
+  local start, ends = string.find(pathReversed, "\\")
+  local trimmedPath = string.sub(path, 1, #path - ends)
+  os.execute("IF NOT EXIST "..trimmedPath.." mkdir "..trimmedPath)
 end
 
-function Build.copyFile(src, dst)
-  os.execute("copy \"" .. src .. "\" \"" .. dst .. "\"")
-end
-
-function Build.copyFolder(src, dst)
-  os.execute("xcopy /E /H /y " .. src .. " " .. dst)
-end
-
-function Build.getAbsolutePath(path)
+function module.getAbsolutePath(path)
   local dirCommand = io.popen("cd")
   return dirCommand:read("*l").."\\"..path
 end
 
-function Build.getRelativePath(path, folder)
-  return path:gsub(folder, "")
+function module.getRelativePath(path, folder)
+  path = path:gsub(folder, "")
+  local char = string.sub(path, 1, 1)
+  if (char == "\\") then
+    -- trim leading slash
+    path = string.sub(path, 2)
+  end
+  return path
 end
 
-function Build.exportAseprite(inputFolder, outputFolder, ignoredLayers, verbose)
-  local dirCommand = io.popen("dir /a-D /S /B \"" .. inputFolder .. "\"")
-  local dirOutput = dirCommand:read("*a")
+-- TODO: need to add this as file processor - does aseprite support returning the file?
+-- function module.exportAseprite(inputFolder, outputFolder, ignoredLayers, verbose)
+--   local dirCommand = io.popen("dir /a-D /S /B \"" .. inputFolder .. "\"")
+--   local dirOutput = dirCommand:read("*a")
 
-  dirCommand = io.popen("cd")
-  local fullInputFolder = dirCommand:read("*l").."\\"..inputFolder
+--   dirCommand = io.popen("cd")
+--   local fullInputFolder = dirCommand:read("*l").."\\"..inputFolder
   
-  for fullPath in dirOutput:gmatch("(.-)\n") do 
-    if Build.getFileExtension(fullPath) ~= ".aseprite" then
-      goto continue
+--   for fullPath in dirOutput:gmatch("(.-)\n") do 
+--     if module.getFileExtension(fullPath) ~= ".aseprite" then
+--       goto continue
+--     end
+
+--     local relativeOutputPath = fullPath:gsub(fullInputFolder, ""):gsub(".aseprite", ".png")
+--     relativeOutputPath = outputFolder .. relativeOutputPath
+
+--     -- create folder(s) first to fix errors when writing lua fila
+--     module.createFolderIfNeeded(relativeOutputPath:match("(.*\\)"))
+
+--     -- TODO: expose param to set aseprite location
+--     local command = "\"C:\\Program Files\\Aseprite\\Aseprite.exe\" -bv "
+--     command = command..fullPath
+--     for i = 1, #ignoredLayers, 1 do
+--       command = command.." --ignore-layer "..ignoredLayers[i]
+--     end
+--     command = command.." --save-as "..relativeOutputPath
+--     io.popen(command, "w")
+
+--     ::continue::
+--   end
+-- end
+
+function module.processFnt(fileContents)
+  capToBmfont()
+end
+
+function module.processLua(fileContents)
+  -- remove comments so preprocess can...process them
+  -- TODO: this is probably slow...is there a better way to handle this?
+  fileContents = fileContents:gsub("--!", "!")
+
+  -- run preprocess magic
+  local processedLua, processedFileInfo = pp.processString {
+    code = fileContents,
+  }
+
+  if verbose then
+    print("Processed " .. fullPath .. " " .. processedFileInfo.processedByteCount .. " bytes")
+  end
+
+  return processedLua
+end
+
+function module.getProjectFolder()
+  local cdCommand = io.popen("cd")
+  return cdCommand:read("*l")
+end
+
+function module.getFiles(path)
+  local dirCommand = io.popen("dir /a-d /s /b \""..path.."\"")
+  local files = dirCommand:read("*a"):gmatch("(.-)\n")
+  local result = {}
+  for path in files do 
+    table.insert(result, path)
+  end
+  return result
+end
+
+function module.processFile(input, output, fileProcessors)
+  local ext = module.getFileExtension(input)
+  local processor = fileProcessors[ext]
+
+  -- if there is a file processor, we want the mode to be in plain text
+  -- other wise mode needs to be binary to copy files like images correctly
+  local writeMode = "w+b"
+  local readMode = "rb"
+  if processor then
+    writeMode = "w+"
+    readMode = "r"
+  end
+
+  local inputFile = io.open(input, readMode)
+  local contents = inputFile:read("a")
+  inputFile:close()
+  
+  if processor then
+    contents = processor(contents)
+  end
+
+  module.createFolderIfNeeded(output)
+  local outputFile = io.open(output, writeMode)
+  outputFile:write(contents)
+  outputFile:close();
+end
+
+function module.processPath(projectFolder, buildFolder, inputPath, outputPath, fileProcessors)
+  local files = module.getFiles(inputPath)
+  if #files == 1 then
+    -- process single file
+    local filePath = files[1]
+    local outputFilePath = projectFolder.."\\"..buildFolder.."\\"..outputPath
+    module.processFile(filePath, outputFilePath, fileProcessors)
+  else
+    -- process files in folder recursively
+    local fullInputPath = projectFolder.."\\"..inputPath
+    for i = 1, #files, 1 do
+      local filePath = files[i]
+      local relativeFilePath = module.getRelativePath(filePath, fullInputPath)
+      local outputFilePath = projectFolder.."\\"..buildFolder.."\\"..outputPath.."\\"..relativeFilePath
+      module.processFile(filePath, outputFilePath, fileProcessors)
     end
-
-    local relativeOutputPath = fullPath:gsub(fullInputFolder, ""):gsub(".aseprite", ".png")
-    relativeOutputPath = outputFolder .. relativeOutputPath
-
-    -- create folder(s) first to fix errors when writing lua fila
-    Build.createFolderIfNeeded(relativeOutputPath:match("(.*\\)"))
-
-    -- TODO: expose param to set aseprite location
-    local command = "\"C:\\Program Files\\Aseprite\\Aseprite.exe\" -bv "
-    command = command..fullPath
-    for i = 1, #ignoredLayers, 1 do
-      command = command.." --ignore-layer "..ignoredLayers[i]
-    end
-    command = command.." --save-as "..relativeOutputPath
-    io.popen(command, "w")
-
-    ::continue::
   end
 end
 
-function Build.processLua(inputFolder, outputFolder, verbose)
-  local dirCommand = io.popen("dir /a-D /S /B \"" .. inputFolder .. "\"")
-  local dirOutput = dirCommand:read("*a")
-
-  dirCommand = io.popen("cd")
-  local fullInputFolder = dirCommand:read("*l").."\\"..inputFolder
-
-  for fullPath in dirOutput:gmatch("(.-)\n") do 
-    if Build.getFileExtension(fullPath) ~= ".lua" then
-      goto continue
-    end
-
-    local relativeOutputPath = fullPath:gsub(fullInputFolder, "")
-    relativeOutputPath = outputFolder .. relativeOutputPath
-
-    -- read original source
-    local file = io.open(fullPath, "r")
-    local rawLua = file:read("a")
-    file:close();
-
-    -- remove comments so preprocess can...process them
-    -- TODO: this is probably slow...is there a better way to handle this?
-    local replacedLua = rawLua:gsub("--!", "!")
-
-    -- run preprocess magic
-    local processedLua, processedFileInfo = pp.processString {
-      code = replacedLua,
-    }
-
-    -- create folder(s) first to fix errors when writing lua fila
-    Build.createFolderIfNeeded(relativeOutputPath:match("(.*\\)"))
-
-    -- save out processed file
-    file = io.open(relativeOutputPath, "w+")
-    file:write(processedLua)
-    file:close();
-
-    if verbose then
-      print("Processed " .. fullPath .. " " .. processedFileInfo.processedByteCount .. " bytes")
-    end
-
-    ::continue::
-  end
-end
-
-function Build.build(options)
+function module.build(options)
   local timeStart = os.clock()
 
   local enableVerbose = options.verbose == true
   local targetPlatform = options.platform
+  local projectFolder = module.getProjectFolder()
+  local processors = options.fileProcessors
 
   -- built in env values
   pp.metaEnvironment.PLAYDATE = targetPlatform == "playdate"
@@ -134,45 +172,21 @@ function Build.build(options)
     end
   end
 
-  local outputFolder = "_dist"
+  local buildFolder = "_game"
   if options.output then
-    outputFolder = options.output
+    buildFolder = options.output
   end
 
   -- nuke old folder
-  os.execute("rmdir "..outputFolder.." /s /q")
-  os.execute("mkdir "..outputFolder)
+  os.execute("rmdir "..buildFolder.." /s /q")
+  os.execute("mkdir "..buildFolder)
 
-  -- process scripts
-  if options.luaFolders then
-    for i = 1, #options.luaFolders, 1 do
-      Build.processLua(options.luaFolders[i][1], outputFolder..options.luaFolders[i][2], enableVerbose)
-    end
-  end
-
-  -- export aseprite files as pngs
-  if options.aseprite and options.aseprite.folders then
-    for i = 1, #options.aseprite.folders, 1 do
-      Build.exportAseprite(options.aseprite.folders[i][1], outputFolder..options.aseprite.folders[i][2], options.aseprite.excludeLayers, enableVerbose)
-    end
-  end
-
-  -- copy folders
-  if options.copyFolders then
-    for i = 1, #options.copyFolders, 1 do
-      Build.copyFolder(options.copyFolders[i][1], outputFolder..options.copyFolders[i][2])
-    end
-  end
-
-  -- copy files
-  if options.copyFiles then
-    for i = 1, #options.copyFiles, 1 do
-      Build.copyFile(options.copyFiles[i][1], outputFolder..options.copyFiles[i][2])
-    end
+  for i = 1, #options.folders, 1 do
+    module.processPath(projectFolder, buildFolder, options.folders[i][1], options.folders[i][2], processors)
   end
 
   local timeEnd = os.clock()
   print("Build completed in "..(timeEnd - timeStart).."ms")
 end
 
-return Build
+return module
