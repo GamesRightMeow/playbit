6b7e34439e7aad6438ec3323e10f5f25ed47dfa9
Alex Larioza <contact@alexlarioza.com>
2022-04-19 21:27:00 -0700

    use lua's random functions

--- playbit/random.lua
+++ playbit/random.lua
@@ -8,35 +8,23 @@ end
 
 --- Returns a random value between 0.0 and 1.0.
 function module.value()
-  --! if LOVE2D then
-  return love.math.random()
-  --! else
-  return 0
-  --! end
+  return math.random()
 end
 
 --- Returns a random integer between min and max.
 function module.intRange(min, max)
-  --! if LOVE2D then
-  -- love does not support floating point numbers
-  return love.math.random(min, max)
-  --! else
-  return 0
-  --! end
+  return math.random(min, max)
 end
 
 --- Returns a random float between min and max, using the specified precision (defaults to 3).
 function module.floatRange(min, max, precision)
-  --! if LOVE2D then
+  -- math.random does not support floating point numbers
 	local precision = precision or 3
 	local num = module.value()
 	local range = math.abs(max - min)
 	local offset = range * num
 	local randomnum = min + offset
 	return math.floor(randomnum * math.pow(10, precision) + 0.5) / math.pow(10, precision)
-  --! else
-  return 0
-  --! end
 end
 
 return module
