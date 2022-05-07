b4308166d8b46581ac95b04d1fc507cd36e941dd
Alex Larioza <contact@alexlarioza.com>
2022-05-06 21:45:00 -0700

    renamed vars to remove row/col confusion

--- playbit/tilemap.lua
+++ playbit/tilemap.lua
@@ -7,48 +7,48 @@ module.__index = meta
 -- TODO: is drawing with a nested array vs 1d array is faster on playdate?
 -- TODO: is the native playdate tilemap faster?
 
-function module.new(spritesheet, rows, columns)
+function module.new(spritesheet, sizeX, sizeY)
   local tilemap = setmetatable({}, meta)
   --! if LOVE2D then
   tilemap.spritesheet = spritesheet
-  tilemap.rows = rows
-  tilemap.length = rows * columns
+  tilemap.sizeX = sizeY
+  tilemap.length = sizeX * sizeY
   tilemap.tiles = {}
   for i = 1, tilemap.length, 1 do
-    tilemap.tiles[i] = 1
+    tilemap.tiles[i] = 0
   end
   --! elseif PLAYDATE then
   local tm = playdate.graphics.tilemap.new()
   tm:setImageTable(spritesheet.imagetable)
-  tm:setSize(rows, columns)
+  tm:setSize(sizeX, sizeY)
   tilemap.tilemap = tm
   --! end
   return tilemap
 end
 
-function meta:setTile(row, col, tile)
+function meta:setTile(x, y, tile)
   --! if LOVE2D then
-  local index = self.rows * (row - 1) + col
+  local index = self.sizeX * (x - 1) + y
   self.tiles[index] = tile
   --! elseif PLAYDATE then
-  self.tilemap:setTileAtPosition(row, col, tile)
+  self.tilemap:setTileAtPosition(x, y, tile)
   --! end
 end
 
-function meta:getTile(row, col)
+function meta:getTile(x, y)
   --! if LOVE2D then
-  local index = self.rows * (row - 1) + col
+  local index = self.sizeX * (x - 1) + y
   return self.tiles[index]
   --! elseif PLAYDATE then
-  return self.tilemap:getTileAtPosition(row, col)
+  return self.tilemap:getTileAtPosition(x, y)
   --! end
 end
 
 function meta:draw(x, y)
   --! if LOVE2D then
   for i = 1, self.length, 1 do
-    local tx = math.floor((i - 1) / self.rows) * self.spritesheet.spriteWidth
-    local ty = math.floor((i - 1) % self.rows) * self.spritesheet.spriteHeight
+    local tx = math.floor((i - 1) / self.sizeX) * self.spritesheet.spriteWidth
+    local ty = math.floor((i - 1) % self.sizeX) * self.spritesheet.spriteHeight
     self.spritesheet:draw(self.tiles[i], tx, ty)
   end
   --! elseif PLAYDATE then
