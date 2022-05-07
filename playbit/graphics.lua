303ce9a52283aebe89e74d3c1f4fe162700dcf45
Alex Larioza <contact@alexlarioza.com>
2022-05-06 21:19:52 -0700

    fixed love2d shader + always uses playdate colors, regardless of source files

--- playbit/graphics.lua
+++ playbit/graphics.lua
@@ -8,32 +8,78 @@ local COLOR_BLACK = { r = 49 / 255, g = 47 / 255, b = 40 / 255 }
 
 module.playbitShader = love.graphics.newShader[[
 extern int mode;
-extern vec3 targetColor;
+
+const vec4 WHITE =        vec4(176.0f / 255.0f, 174.0f / 255.0f, 167.0f / 255.0f, 1);
+const vec4 BLACK =        vec4( 49.0f / 255.0f,  47.0f / 255.0f,  40.0f / 255.0f, 1);
+const vec4 TRANSPARENT =  vec4(        0,         0,         0, 0);
+
+vec3 rgb2hsv(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
 
 vec4 effect(vec4 color, Image tex, vec2 texcoord, vec2 screen_coords )
 {
   vec4 outputcolor = Texel(tex, texcoord) * color;
-  if (mode == 0)
-  {
-    return outputcolor;
-  }
-  else if (mode == 1)
+  if (mode == 0) // --------------- "copy"
   {
     if (outputcolor.a > 0)
     {
-      // playdate white
-      return vec4(targetColor.r, targetColor.g, targetColor.b, 1);
+      // choose white or black based on saturation
+      float saturation = rgb2hsv(vec3(outputcolor)).z;
+      // ideally this value is 0.5f (halfway) not sure why this doesn't work?
+      if (saturation >= 0.45f)
+      {
+        return WHITE;
+      }
+      else
+      {
+        return BLACK;
+      }
     }
     else
     {
-      // transparent
-      return vec4(0, 0, 0, 0);
+      // transparent pixel
+      return TRANSPARENT;
+    }
+  }
+  else if (mode == 1) // ---------- "fillWhite"
+  {
+    if (outputcolor.a > 0)
+    {
+      // replace with playdate white
+      return WHITE;
+    }
+    else
+    {
+      // leave transparent pixel
+      return TRANSPARENT;
+    }
+  }
+  else if (mode == 2) // ---------- "fillBlack"
+  {
+    if (outputcolor.a > 0)
+    {
+      // replace with playdate black
+      return BLACK;
+    }
+    else
+    {
+      // leave transparent pixel
+      return TRANSPARENT;
     }
   }
 }
 ]]
 
 module.drawOffset = { x = 0, y = 0}
+module.drawColor = COLOR_WHITE
 
 -- don't want to deal with these, so just reason one
 module.quad = love.graphics.newQuad(0, 0, 1, 1, 1, 1)
@@ -85,8 +131,10 @@ end
 function module.setColor(color)
   --! if LOVE2D then
   if color == 1 then
+    module.drawColor = COLOR_WHITE
     love.graphics.setColor(COLOR_WHITE.r, COLOR_WHITE.g, COLOR_WHITE.b, 1)
   else
+    module.drawColor = COLOR_BLACK
     love.graphics.setColor(COLOR_BLACK.r, COLOR_BLACK.g, COLOR_BLACK.b, 1)
   end
   --! elseif PLAYDATE then
@@ -101,8 +149,6 @@ end
 -- "copy", "inverted", "XOR", "NXOR", "whiteTransparent", "blackTransparent", "fillWhite", or "fillBlack".
 function module.setImageDrawMode(mode)
   --! if LOVE2D then
-  -- TODO: set shader for images?
-  -- TODO: handle other states?
   module.drawMode = mode
   --! elseif PLAYDATE then
   playdate.graphics.setImageDrawMode(mode)
@@ -111,18 +157,14 @@ end
 
 --! if LOVE2D then
 local function setLoveDrawMode()
-  if module.drawMode == "fillWhite" then
+  if module.drawMode == "copy" then
+    module.playbitShader:send("mode", 0)
+  elseif module.drawMode == "fillWhite" then
     module.playbitShader:send("mode", 1)
-    module.playbitShader:sendColor("targetColor", { COLOR_WHITE.r, COLOR_WHITE.g, COLOR_WHITE.b })
   elseif module.drawMode == "fillBlack" then
-    module.playbitShader:send("mode", 1)
-    module.playbitShader:sendColor("targetColor", { COLOR_BLACK.r, COLOR_BLACK.g, COLOR_BLACK.b })
+    module.playbitShader:send("mode", 2)
   end
   -- TODO: other fill modes
-end
-
-local function resetLoveDrawMode()
-  module.playbitShader:send("mode", 0)
 end
 --! end
 
@@ -186,7 +228,7 @@ function module.texture(image, x, y)
   love.graphics.setColor(1, 1, 1, 1)
   setLoveDrawMode()
   love.graphics.draw(image.data, x, y)
-  resetLoveDrawMode()
+  love.graphics.setColor(module.drawColor.r, module.drawColor.g, module.drawColor.b, 1)
   --! elseif PLAYDATE then
   image.data:draw(x, y)
   --! end
@@ -200,7 +242,7 @@ function module.textureQuad(image, x, y, qx, qy, qw, qh)
   module.quad:setViewport(qx, qy, qw, qh, image:getWidth(), image:getHeight())
   setLoveDrawMode()
   love.graphics.draw(image.data, module.quad, x, y)
-  resetLoveDrawMode()
+  love.graphics.setColor(module.drawColor.r, module.drawColor.g, module.drawColor.b, 1)
   --! elseif PLAYDATE then
   -- TODO: bug in playdate SDK where draw offset affect source rect
   -- https://devforum.play.date/t/image-drawoffset-affects-sourcerect-instead-of-location/3778/6
