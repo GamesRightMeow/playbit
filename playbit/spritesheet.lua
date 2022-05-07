f5f2425b6bf2d3aa62ef50a99655c0464d2ab511
Alex Larioza <contact@alexlarioza.com>
2022-05-06 20:33:11 -0700

    use native tilemap and image table on playdate

--- playbit/spritesheet.lua
+++ playbit/spritesheet.lua
@@ -4,24 +4,29 @@ local meta = {}
 meta.__index = meta
 module.__index = meta
 
-function module.new(image, spriteWidth, spriteHeight)
-  local spritesheet = {
-    image = image,
-    imageWidth = image:getWidth(),
-    imageHeight = image:getHeight(),
-    rows = image:getWidth() / spriteWidth,
-    columns = image:getHeight() / spriteHeight,
-    spriteWidth = spriteWidth,
-    spriteHeight = spriteHeight,
-  }
-  setmetatable(spritesheet, meta)
+function module.new(path, spriteWidth, spriteHeight)
+  local spritesheet = setmetatable({}, meta)
+  --! if LOVE2D then
+  local image = pb.image.new(path.."-table-"..spriteWidth.."-"..spriteHeight)
+  spritesheet.image = image
+  spritesheet.rows = image:getWidth() / spriteWidth
+  spritesheet.columns = image:getHeight() / spriteHeight
+  spritesheet.spriteWidth = spriteWidth
+  spritesheet.spriteHeight = spriteHeight
+  --! elseif PLAYDATE then
+  spritesheet.imagetable = playdate.graphics.imagetable.new("textures/tiles")
+  --! end
   return spritesheet
 end
 
 function meta:draw(n, x, y)
+  --! if LOVE2D then
   local qx = math.floor((n - 1) % self.rows) * self.spriteHeight
   local qy = math.floor((n - 1) / self.rows) * self.spriteWidth
   pb.graphics.textureQuad(self.image, x, y, qx, qy, self.spriteWidth, self.spriteHeight)
+  --! elseif PLAYDATE then
+  self.imagetable:drawImage(n, x, y)
+  --! end
 end
 
 return module
--- playbit/tilemap.lua
+++ playbit/tilemap.lua
@@ -8,37 +8,52 @@ module.__index = meta
 -- TODO: is the native playdate tilemap faster?
 
 function module.new(spritesheet, rows, columns)
-  local tilemap = { 
-    spritesheet = spritesheet,
-    rows = rows, 
-    length = rows * columns,
-    tiles = {},
-  }
-  setmetatable(tilemap, meta)
-  
+  local tilemap = setmetatable({}, meta)
+  --! if LOVE2D then
+  tilemap.spritesheet = spritesheet
+  tilemap.rows = rows
+  tilemap.length = rows * columns
+  tilemap.tiles = {}
   for i = 1, tilemap.length, 1 do
     tilemap.tiles[i] = 1
   end
-
+  --! elseif PLAYDATE then
+  local tm = playdate.graphics.tilemap.new()
+  tm:setImageTable(spritesheet.imagetable)
+  tm:setSize(rows, columns)
+  tilemap.tilemap = tm
+  --! end
   return tilemap
 end
 
 function meta:setTile(row, col, tile)
+  --! if LOVE2D then
   local index = self.rows * (row - 1) + col
   self.tiles[index] = tile
+  --! elseif PLAYDATE then
+  self.tilemap:setTileAtPosition(row, col, tile)
+  --! end
 end
 
 function meta:getTile(row, col)
+  --! if LOVE2D then
   local index = self.rows * (row - 1) + col
   return self.tiles[index]
+  --! elseif PLAYDATE then
+  return self.tilemap:getTileAtPosition(row, col)
+  --! end
 end
 
 function meta:draw(x, y)
+  --! if LOVE2D then
   for i = 1, self.length, 1 do
     local tx = math.floor((i - 1) / self.rows) * self.spritesheet.spriteWidth
     local ty = math.floor((i - 1) % self.rows) * self.spritesheet.spriteHeight
     self.spritesheet:draw(self.tiles[i], tx, ty)
   end
+  --! elseif PLAYDATE then
+  self.tilemap:draw(x, y)
+  --! end
 end
 
 return module
