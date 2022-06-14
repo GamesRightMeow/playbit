1f1b31f181ff5dd279915559e8a6a51ff359a44e
Alex Larioza <contact@alexlarioza.com>
2022-06-13 20:54:55 -0700

    tilemap now implements its own draw function instead of calling into image:draw() to ensure updateContext is only called once per map, not per tile

--- playdate/tilemap.lua
+++ playdate/tilemap.lua
@@ -43,12 +43,28 @@ function meta:getTileAtPosition(x, y)
 end
 
 function meta:draw(x, y)
+  local r, g, b = love.graphics.getColor()
+  love.graphics.setColor(1, 1, 1, 1)
+
+  local w, h = self.imagetable.image:getSize()
+  local frameWidth = self.imagetable.frameWidth
+  local frameHeight = self.imagetable.frameHeight
+  local imageRows = self.imagetable.rows
+
   for tx = 1, self.width, 1 do
     for ty = 1, self.height, 1 do
       local index = self.tiles[tx][ty]
-      local dx = x + ((tx - 1) * self.imagetable.frameWidth)
-      local dy = y + ((ty - 1) * self.imagetable.frameHeight)
-      self.imagetable:drawImage(index, dx, dy)
+      local dx = x + ((tx - 1) * frameWidth)
+      local dy = y + ((ty - 1) * frameHeight)
+      local qx = math.floor((index - 1) % imageRows) * frameHeight
+      local qy = math.floor((index - 1) / imageRows) * frameWidth
+      playdate.graphics._quad:setViewport(qx, qy, frameWidth, frameHeight, w, h)
+      love.graphics.draw(self.imagetable.image.data, playdate.graphics._quad, dx, dy)
     end
   end
+
+  love.graphics.setColor(r, g, b, 1)
+
+  -- important for perf that this is called after all tiles are drawn
+  playdate.graphics._updateContext()
 end
