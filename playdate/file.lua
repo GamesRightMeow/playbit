300b7f17137d904c4c6ad833a1bf01fc553cffe2
Alex Larioza <contact@alexlarioza.com>
2022-06-13 21:59:28 -0700

    added file apis

--- playdate/file.lua
+++ playdate/file.lua
@@ -1,5 +1,51 @@
 local module = {}
 playdate.file = module
 
--- TODO:
+local meta = {}
+meta.__index = meta
+module.__index = meta
 
+module.kFileRead = 3
+module.kFileWrite = 4
+module.kFileAppend = 8
+
+function module.listFiles(path)
+  path = path or "/"
+  return love.filesystem.getDirectoryItems(path)
+end
+
+function module.open(path, mode)
+  mode = mode or module.kFileRead
+
+  local data = love.filesystem.newFile(path)
+  if mode == module.kFileRead then
+    data:open("r")
+  elseif mode == module.kFileWrite then
+    data:open("w")
+  elseif mode == module.kFileAppend then
+    data:open("a")
+  end
+  
+  local file = setmetatable({}, meta)
+  file._data = data
+  file._lastLine = data:lines()
+  return file
+end
+
+function meta:close()
+  self._data:close()
+end
+
+function meta:readline()
+  if self._data:isEOF() then
+    return nil
+  end
+
+  local line = self._lastLine()
+  if not line then
+    -- at the EOF, love2d returns nothing, but PD returns nil
+    return nil
+  end
+
+  return line
+end
