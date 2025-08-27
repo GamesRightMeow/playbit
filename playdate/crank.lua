9dbf89e2334838171c20527e70aa0cb97f7c567b
Alex Larioza <contact@gamesrightmeow.com>
2025-08-27 08:27:33 -0700

    implemented getCrankTicks

--- playdate/crank.lua
+++ playdate/crank.lua
@@ -1,5 +1,27 @@
 local module = playdate or {}
 
+local lastCrankPos = 0
+
 function module.getCrankTicks(ticksPerRevolution)
-  error("[ERR] playdate.getCrankTicks() is not yet implemented.")
+  local degreesPerTick = 360 / ticksPerRevolution
+  local crankPos = playdate.getCrankPosition()
+
+  local thisTick = math.ceil(crankPos / degreesPerTick)
+
+  -- handle crossing 360/0 border
+  local delta = math.abs(crankPos - lastCrankPos)
+  if delta > 180 then
+    if lastCrankPos >= 180 then
+        lastCrankPos = lastCrankPos - 360
+    else
+        lastCrankPos = lastCrankPos + 360
+    end
+  end
+  local lastTick = math.ceil(lastCrankPos / degreesPerTick)
+  
+  local tickDelta = thisTick - lastTick
+
+  lastCrankPos = crankPos
+
+  return tickDelta
 end
